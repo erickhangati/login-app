a8a758245ceed7e62afc6e37c1461626755d26d4
Eric Khangati <ekhangati@gmail.com>
2023-07-26 05:35:42 +0300

    Connected to MongoDB

---
+++ server/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const connect = require('./db/connect.js');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(morgan('tiny'));
+app.disable('x-powered-by');
+
+const port = 8080;
+
+app.get('/', (req, res) => {
+  res.status(201).json('Home server page');
+});
+
+/** start server only when db connected */
+connect()
+  .then(() => {
+    try {
+      app.listen(port, () => {
+        console.log(`Server running on port ${port}`);
+      });
+    } catch (error) {
+      console.log('Cannot connect to the server');
+    }
+  })
+  .catch((error) => {
+    console.log('Invalid database connection');
+  });
