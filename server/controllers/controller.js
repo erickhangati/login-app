7eb1382f2dc15e48b9002c7b615dcb3c84733a47
Eric Khangati <ekhangati@gmail.com>
2023-07-27 10:10:09 +0300

    Added controllers to end points

---
+++ server/controllers/controller.js
@@ -0,0 +1,57 @@
+/** POST: http://localhost:8080/api/register 
+ * @param : {
+  "username" : "example123",
+  "password" : "admin123",
+  "email": "example@gmail.com",
+  "firstName" : "bill",
+  "lastName": "william",
+  "mobile": 8009860560,
+  "address" : "Apt. 556, Kulas Light, Gwenborough",
+  "profile": ""
+}
+*/
+exports.register = async (req, res) => {
+  res.json('Register route');
+};
+
+/** POST: http://localhost:8080/api/login 
+ * @param: {
+  "username" : "example123",
+  "password" : "admin123"
+}
+*/
+exports.login = async (req, res) => {
+  res.json('Login route');
+};
+
+/** GET: http://localhost:8080/api/user/example123 */
+exports.getUser = async (req, res) => {
+  res.json('Get user route');
+};
+
+/** PUT: http://localhost:8080/api/update-user */
+exports.updateUser = async (req, res) => {
+  res.json('Update user route');
+};
+
+/** GET: http://localhost:8080/api/generate-otp */
+exports.generateOTP = async (req, res) => {
+  res.json('Generate OTP route');
+};
+
+/** GET: http://localhost:8080/api/verify-otp */
+exports.verifyOTP = async (req, res) => {
+  res.json('Verify OTP route');
+};
+
+// successfully redirect user when OTP is valid
+/** GET: http://localhost:8080/api/create-reset-session */
+exports.createResetSession = async (req, res) => {
+  res.json('Create reset session route');
+};
+
+// updat ethe password when we have valid session
+/** PUT: http://localhost:8080/api/reset-password */
+exports.resetPassword = async (req, res) => {
+  res.json('Reset password route');
+};
--- server/router/routes.js
+++ server/router/routes.js
@@ -1,25 +1,33 @@
 const express = require('express');
+const {
+  register,
+  login,
+  getUser,
+  generateOTP,
+  verifyOTP,
+  createResetSession,
+  updateUser,
+  resetPassword,
+} = require('../controllers/controller');
 
 const router = express.Router();
 
 /** POST methods */
-router.route('/register').post((req, res) => res.json('Register route')); // register user
+router.route('/register').post(register); // register user
 router
   .route('/register-mail')
   .post((req, res) => res.json('Register mail route')); // send email
 router.route('/auth').post((req, res) => res.json('Auth route')); // authenticate user
-router.route('/login').post((req, res) => res.json('Login route')); // login in app
+router.route('/login').post(login); // login in app
 
 /** GET methodes */
-router.route('/user/:username').get((req, res) => res.json('User route')); // get user with username
-router.route('/generate-otp').get((req, res) => res.json('User route')); // generate random OTP
-router.route('/verify-otp').get((req, res) => res.json('User route')); // verify generated OTP
-router.route('/create-reset-session').get((req, res) => res.json('User route')); // reset all the variables
+router.route('/user/:username').get(getUser); // get user with username
+router.route('/generate-otp').get(generateOTP); // generate random OTP
+router.route('/verify-otp').get(verifyOTP); // verify generated OTP
+router.route('/create-reset-session').get(createResetSession); // reset all the variables
 
 /** PUT methods */
-router.route('/update-user').put((req, res) => res.json('Update user route')); // update user profile
-router
-  .route('/reset-password')
-  .put((req, res) => res.json('Reset password route')); // reset password
+router.route('/update-user').put(updateUser); // update user profile
+router.route('/reset-password').put(resetPassword); // reset password
 
 module.exports = router;
