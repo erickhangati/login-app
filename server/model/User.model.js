e0cfb932723e852a0948c160f10b5dd176ad11e7
Eric Khangati <ekhangati@gmail.com>
2023-07-27 10:35:17 +0300

    Added user model

---
+++ server/model/User.model.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+exports.UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, 'Please provide unique Username'],
+    unique: [true, 'Username exists'],
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    unique: false,
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide a unique email'],
+    unique: true,
+  },
+  firstName: { type: String },
+  lastName: { type: String },
+  mobile: { type: Number },
+  address: { type: String },
+  profile: { type: String },
+});
+
+module.exports = mongoose.model.Users || mongoose.model('User', UserSchema);
